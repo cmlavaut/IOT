aec2528f413886f67a227a7c93a721f740cbf8a3
Camila <cmlavaut96@gmail.com>
2023-02-03 17:01:40 -0600

    primer int

---
+++ led+button.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as gpio
+import time
+
+def main():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(8, gpio.OUT)
+    gpio.setup(10, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+    
+    while True:
+        if GPIO.input(10):
+            gpio.output(8,1)
+            time.sleep(0.1)
+        else:
+            gpio.output(8,0)
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
