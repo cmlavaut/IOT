b42df7ccd00e08ad6fae051172b8462338ee7761
Camila <cmlavaut96@gmail.com>
2023-02-06 11:40:19 -0600

    first

---
+++ Serial1.py
@@ -0,0 +1,43 @@
+import serial
+#import RPi.GPIO as gpio
+import time
+
+def main():
+    arduino = serial.Serial()
+    arduino.port = "/dev/ttyACM0"
+    arduino.baudrate = 9600
+    arduino.open()
+    #gpio.setmode(gpio.BOARD)
+    #gpio.setup(8,gpio.OUT)
+
+    while True:
+        sensor = arduino.readline()
+        sensor = sensor.decode()
+        value= sensor.split()
+        print(value)
+       
+       """
+        try:
+            value = float(sensor[6:11])
+            if (value > 25.0):
+                gpio.output(8,1)
+            else:
+                gpio.output(8,0)
+            print(value)
+        except:
+            pass
+        """    
+
+    
+
+        
+        time.sleep(1)
+        
+    arduino.close()
+
+
+  
+
+if __name__ == "__main__":
+
+    main()
