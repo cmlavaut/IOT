c5b8fc6b7707cf452eb4f8073d5b0b799a3e90da
Camila <cmlavaut96@gmail.com>
2023-02-22 16:36:45 -0600

    tarea class6

--- lecturaprueba.py
+++ lecturaprueba.py
@@ -2,35 +2,75 @@ import serial
 #import RPi.GPIO as gpio
 import time
 import sys
+import pandas as pd
+from datetime import datetime
+
+
+dicc = {
+        "humedad_sueloOut" : [],
+        "humedad_ambOut" : [],
+        "temperaturaOut" :[],
+        }
+tabla = pd.DataFrame.from_dict(dicc)
+tabla.to_csv('./tabla.csv',index= False)
 
 def main():
     arduino = serial.Serial()
-    arduino2= serial.Serial()
+    #arduino2= serial.Serial()
     try:
         puerto1= sys.argv[1]
-        puerto2= sys.argv[2]
+        #puerto2= sys.argv[2]
         arduino.port= 'COM' + puerto1
-        arduino2.port = 'COM' + puerto2
+        #arduino2.port = 'COM' + puerto2
         arduino.baudrate = 115200
-        arduino2.baudrate = 115200
+        #arduino2.baudrate = 115200
         arduino.open()
-        arduino2.open()
+        #arduino2.open()
     except:
         try:
             arduino.port = "/dev/ttyACM0"
+
             arduino.baudrate = 9600
             print(arduino.port)
             arduino.open()
         except:
             print("nothing conected")
             quit()
-
-     
+    
     while True:
         arduino.flushInput()
         sensor = arduino.readline()
         sensor = sensor.decode()
         value= sensor.split()
+        value = value[2:5]
+        print(value)
+        guardar(value)
+        time.sleep(3)
+    arduino.close()
+ 
+
+
+
+
+
+def guardar(valor):
+        tabla = pd.read_csv('./tabla.csv')
+        now = datetime.now()
+        now_fecha = now.strftime("%d %m %y")
+        now_hora = now.strftime("%H:%M:%S")
+        try:
+            tabla.insert(0, "Fecha", now_fecha)
+            tabla.insert(1, "Hora", now_hora)
+        except:
+            print("ya existe columnas de hora y fecha")
+            pass
+        datos = [now_fecha, now_hora]
+        datos.extend(valor)
+        tabla.loc[tabla.shape[0]]= datos
+        tabla.to_csv('./tabla.csv',index= False)
+
+
+        """
         arduino2.flushInput()
         sensor11 = arduino2.readline()
                 sensor11 = sensor11.decode()
@@ -50,12 +90,7 @@ def main():
             print("valores incorrectos")
     
        
-            
-        time.sleep(3)
-        
-    arduino.close()
+           """ 
 
-
- __name__ == "__main__":
-
+if __name__ == "__main__":
     main()
